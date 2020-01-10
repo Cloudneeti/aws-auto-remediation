8b084f0920d63bfc6dc863e7a948424d25ad2f3f
Bhushan Bhat <bhushanb@cloudneeti.com>
2020-01-10 19:23:40 +0530

    updated init

--- remediation-functions/rds_cluster/__init__.py
+++ remediation-functions/rds_cluster/__init__.py
@@ -1,1 +1,1 @@
-__all__ = [ "rdscluster_autopause", "rdscluster_backupretention", "rdscluster_copytagstosnapshot", "rdscluster_deletion_protection"]
+__all__ = [ "rdscluster_autopause", "rdscluster_backupretention", "rdscluster_copytagstosnapshot", "rdscluster_deletion_protection","rdscluster_logsenabled"]
