f38e92e0b5719e62e94a27f4c682c8fa96698b4d
Bhushan Bhat <bhushanb@cloudneeti.com>
2020-01-20 18:16:42 +0530

    dynamodb, ec2, s3 policies

---
+++ remediation-functions/ebs/dynamodb_suborchestrator.py
@@ -0,0 +1,141 @@
+'''
+dynamodb sub-orchestrator function
+'''
+
+import json
+import boto3
+import common
+from botocore.exceptions import ClientError
+from dynamodb import *
+
+def lambda_handler(event, context):
+    global aws_access_key_id, aws_secret_access_key, aws_session_token, CustAccID, Region
+    
+    try:
+        PolicyId = json.loads(event["body"])["PolicyId"]
+    except:
+        PolicyId = ''
+        pass
+
+    if not PolicyId:
+        print("Executing auto-remediation")
+        try:  # common code
+            CustAccID, role_arn = common.getRoleArn_cwlogs(event)
+            aws_access_key_id, aws_secret_access_key, aws_session_token = common.getCredentials(role_arn)
+        except ClientError as e:
+            print(e)
+            return {  
+                'statusCode': 400,
+                'body': str(e)
+            }
+        except Exception as e:
+            print(e)
+            return {
+                'statusCode': 400,
+                'body': str(e)
+            }
+
+        try:
+            Region = event["Region"]
+            table_name = event["table_name"]
+            records_json = json.loads(event["policies"])
+            records = records_json["RemediationPolicies"]
+        except:
+            Region = ""
+
+        try:
+            # Establish a session with the portal
+            dynamodb = boto3.client('dynamodb', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,aws_session_token=aws_session_token,region_name=Region)  
+        except ClientError as e:
+            print(e)
+            return {  
+                'statusCode': 400,
+                'body': str(e)
+            }
+        except Exception as e:
+            print(e)
+            return {
+                'statusCode': 400,
+                'body': str(e)
+            }
+        
+        if "DynamoDbContinuousBackup" in str(records):
+            try:
+                dynamodb_continuous_backups.run_remediation(dynamodb,table_name)
+            except ClientError as e:
+                print(e)
+                return {  
+                    'statusCode': 400,
+                    'body': str(e)
+                }
+            except Exception as e:
+                print(e)
+                return {
+                    'statusCode': 400,
+                    'body': str(e)
+                }   
+        
+        print('remediated-' + table_name)
+        #returning the output Array in json format
+        return {  
+            'statusCode': 200,
+            'body': json.dumps('remediated-' + table_name)
+        }
+
+    else:
+        print("CN-portal triggered remediation")
+        try:
+            CustAccID, role_arn = common.getRoleArn(event)
+            aws_access_key_id, aws_secret_access_key, aws_session_token = common.getCredentials(role_arn)
+        except ClientError as e:
+            print(e)
+            return {  
+                'statusCode': 400,
+                'body': str(e)
+            }
+        except Exception as e:
+            print(e)
+            return {
+                'statusCode': 400,
+                'body': str(e)
+            }
+
+        try:
+            Region_name = json.loads(event["body"])["Region"]
+            Region = common.getRegionName(Region_name)
+            table_name = json.loads(event["body"])["ResourceName"]
+        except:
+            Region = ""
+
+        try:
+            # Establish a session with the portal
+            dynamodb = boto3.client('dynamodb', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,aws_session_token=aws_session_token,region_name=Region)  
+        except ClientError as e:
+            print(e)
+            return {  
+                'statusCode': 400,
+                'body': str(e)
+            }
+        except Exception as e:
+            print(e)
+            return {
+                'statusCode': 400,
+                'body': str(e)
+            }
+
+        try:
+            if PolicyId == "DynamoDbContinuousBackup":  
+                responseCode,output = dynamodb_continuous_backups.run_remediation(dynamodb,table_name)
+        
+        except ClientError as e:
+            responseCode = 400
+            output = "Unable to remediate classic load balancer: " + str(e)
+        except Exception as e:
+            responseCode = 400
+            output = "Unable to remediate classic load balancer: " + str(e)
+
+            # returning the output Array in json format
+        return {  
+            'statusCode': responseCode,
+            'body': output
+        }
