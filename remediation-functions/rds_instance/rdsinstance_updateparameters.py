e74ee1a68623b02d35001f7e00441ed43bd04e00
Bhushan Bhat <bhushanb@cloudneeti.com>
2020-01-30 19:27:32 +0530

    added route53 service to framework

---
+++ remediation-functions/rds_instance/rdsinstance_updateparameters.py
@@ -0,0 +1,101 @@
+'''
+Use latest block encryption algorithms for RDS MySQL Instance
+Enable FIPS standards for ssl encryption on the server side for RDS MySQL Instance
+'''
+
+from botocore.exceptions import ClientError
+
+def run_remediation(rds, RDSInstanceName):
+    print("Executing RDS Instance remediation")  
+    ssl_fips = 1
+    enable_aes_latest = 2
+    parametervalues = []
+    parametergroups = []
+    db_engine_flag = 0
+    db_version_flag = 0
+    ParameterFIPS = ['1','STRICT']
+    ParameterBlockEncryption = ['aes-192-ecb','aes-256-ecb','aes-192-cbc','aes-256-cbc'] 
+    try:
+        dbinstance_details = rds.describe_db_instances(DBInstanceIdentifier=RDSInstanceName)['DBInstances']
+        if dbinstance_details[0]['Engine'] == 'mysql':
+            db_engine_flag = 1
+        if dbinstance_details[0]['EngineVersion'] in ['mysql8.0.11','mysql8.0.13','mysql8.0.15','mysql8.0.16']:
+            db_version_flag = 1
+        RDSParameterGroup = Instance_Det[0]['DBParameterGroups']
+    except ClientError as e:
+        responseCode = 400
+        output = "Unexpected error: " + str(e)
+    except Exception as e:
+        responseCode = 400
+        output = "Unexpected error: " + str(e)
+    try:
+        for parameter in RDSParameterGroup:
+            if parameter['DBParameterGroupName'] and str(parameter['DBParameterGroupName']).find('default') == -1:
+                parametergroups.append(parameter['DBParameterGroupName'])
+        for parameter in parametergroups:
+            Parameter_Det = rds.describe_db_parameters(DBParameterGroupName = parameter, Source='user')['Parameters']
+            for protocol in Parameter_Det:
+                #Checking for MySQL Instance to use latest  block encryption mode for block-based algorithms   
+                if protocol['ParameterName'] == 'block_encryption_mode':
+                    if str(protocol['ParameterValue']) and str(protocol['ParameterValue']) in ParameterBlockEncryption:
+                        enable_aes_latest = 0
+                
+                #Checking for MySQL Instance to enable FIPS mode on the server side   
+                if protocol['ParameterName'] == 'ssl_fips_mode':
+                    if str(protocol['ParameterValue']) and str(protocol['ParameterValue']) in ParameterFIPS:
+                        ssl_fips = 0
+    except ClientError as e:
+        responseCode = 400
+        output = "Unexpected error: " + str(e)
+    except Exception as e:
+        responseCode = 400
+        output = "Unexpected error: " + str(e)
+    
+    if db_engine_flag and db_version_flag:       
+        if 1 in parametervalues:
+            try:
+                result = rds.modify_db_parameter_group(DBParameterGroupName= mysql_parameter_group,Parameters=[{'ParameterName': 'ssl_fips_mode','ParameterValue': '1','Description': 'enable FIPS mode on the server side','ApplyMethod': 'immediate'}])
+
+                responseCode = result['ResponseMetadata']['HTTPStatusCode']
+                if responseCode >= 400:
+                    output = "Unexpected error: %s \n" % str(result)
+                else:
+                    output = "Enabled FIPS standards for ssl encryption on the server side for RDS MySQL Instance : %s \n" % RDSInstanceName
+                        
+            except ClientError as e:
+                responseCode = 400
+                output = "Unexpected error: " + str(e)
+                print(output)
+            except Exception as e:
+                responseCode = 400
+                output = "Unexpected error: " + str(e)
+                print(output)
+        elif 2 in parametervalues:
+            try:
+                result = rds.modify_db_parameter_group(DBParameterGroupName= mysql_parameter_group,Parameters=[{'ParameterName': 'block_encryption_mode','ParameterValue': 'aes-256-cbc','Description': 'use latest  block encryption mode','ApplyMethod': 'immediate'}])
+
+                responseCode = result['ResponseMetadata']['HTTPStatusCode']
+                if responseCode >= 400:
+                    output = "Unexpected error: %s \n" % str(result)
+                else:
+                    output = "Enabled use latest block encryption algorithms for RDS MySQL Instance : %s \n" % RDSInstanceName
+                        
+            except ClientError as e:
+                responseCode = 400
+                output = "Unexpected error: " + str(e)
+                print(output)
+            except Exception as e:
+                responseCode = 400
+                output = "Unexpected error: " + str(e)
+                print(output)
+        else:
+            responseCode=200
+            output='Data-tier security group already configured for RDS Instance : '+RDSInstanceName
+            print(output)
+    else:
+        responseCode=200
+        output='These Configuration are not supported for rds instance please use upgraded mysql instance : '+RDSInstanceName
+        print(output)
+
+    print(str(responseCode)+'-'+output)
+    return responseCode,output
---
+++ remediation-functions/route53_domain/route53domain_suborchestrator.py
@@ -0,0 +1,141 @@
+'''
+Route53 service domain name sub-orchestrator function
+'''
+
+import json
+import boto3
+import common
+from botocore.exceptions import ClientError
+from kms import *
+
+def lambda_handler(event, context):
+    global aws_access_key_id, aws_secret_access_key, aws_session_token, CustAccID, Region
+    
+    try:
+        PolicyId = json.loads(event["body"])["PolicyId"]
+    except:
+        PolicyId = ''
+        pass
+
+    if not PolicyId:
+        print("Executing auto-remediation")
+        try:  # common code
+            CustAccID, role_arn = common.getRoleArn_cwlogs(event)
+            aws_access_key_id, aws_secret_access_key, aws_session_token = common.getCredentials(role_arn)
+        except ClientError as e:
+            print(e)
+            return {  
+                'statusCode': 400,
+                'body': str(e)
+            }
+        except Exception as e:
+            print(e)
+            return {
+                'statusCode': 400,
+                'body': str(e)
+            }
+
+        try:
+            Region = event["Region"]
+            DomainName = event["DomainName"]
+            records_json = json.loads(event["policies"])
+            records = records_json["RemediationPolicies"]
+        except:
+            Region = ""
+
+        try:
+            # Establish a session with the portal
+            route53domains = boto3.client('route53domains', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,aws_session_token=aws_session_token,region_name=Region)  
+        except ClientError as e:
+            print(e)
+            return {  
+                'statusCode': 400,
+                'body': str(e)
+            }
+        except Exception as e:
+            print(e)
+            return {
+                'statusCode': 400,
+                'body': str(e)
+            }
+        
+        if "KMSKeyRotation" in str(records):
+            try:
+                kms_enable_key_rotation.run_remediation(route53domains,DomainName)
+            except ClientError as e:
+                print(e)
+                return {  
+                    'statusCode': 400,
+                    'body': str(e)
+                }
+            except Exception as e:
+                print(e)
+                return {
+                    'statusCode': 400,
+                    'body': str(e)
+                }   
+        
+        print('remediated-' + DomainName)
+        #returning the output Array in json format
+        return {  
+            'statusCode': 200,
+            'body': json.dumps('remediated-' + DomainName)
+        }
+
+    else:
+        print("CN-portal triggered remediation")
+        try:
+            CustAccID, role_arn = common.getRoleArn(event)
+            aws_access_key_id, aws_secret_access_key, aws_session_token = common.getCredentials(role_arn)
+        except ClientError as e:
+            print(e)
+            return {  
+                'statusCode': 400,
+                'body': str(e)
+            }
+        except Exception as e:
+            print(e)
+            return {
+                'statusCode': 400,
+                'body': str(e)
+            }
+
+        try:
+            Region_name = json.loads(event["body"])["Region"]
+            Region = common.getRegionName(Region_name)
+            DomainName = json.loads(event["body"])["ResourceName"]
+        except:
+            Region = ""
+
+        try:
+            # Establish a session with the portal
+            route53domains = boto3.client('route53domains', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,aws_session_token=aws_session_token,region_name=Region)  
+        except ClientError as e:
+            print(e)
+            return {  
+                'statusCode': 400,
+                'body': str(e)
+            }
+        except Exception as e:
+            print(e)
+            return {
+                'statusCode': 400,
+                'body': str(e)
+            }
+
+        try:
+            if PolicyId == "KMSKeyRotation":  
+                responseCode,output = kms_enable_key_rotation.run_remediation(route53domains,DomainName)
+        
+        except ClientError as e:
+            responseCode = 400
+            output = "Unable to remediate bucket: " + str(e)
+        except Exception as e:
+            responseCode = 400
+            output = "Unable to remediate KMS Key: " + str(e)
+
+            # returning the output Array in json format
+        return {  
+            'statusCode': responseCode,
+            'body': output
+        }
